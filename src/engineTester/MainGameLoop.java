c3bd98a8b53d2fff6611723cea06ffa7d062f3d7
YunusBALCI <ynsbalci@outlook.com>
2018-05-05 21:52:42 +0300

    5 commited

--- src/engineTester/MainGameLoop.java
+++ src/engineTester/MainGameLoop.java
@@ -1,56 +1,50 @@
 package engineTester;
-
+ 
 import org.lwjgl.opengl.Display;
-
+import org.lwjgl.opengl.GL11;
+ 
 import renderEngine.DisplayManager;
 import renderEngine.Loader;
 import renderEngine.RawModel;
 import renderEngine.Renderer;
-
-/**
- * This class contains the main method and is used to test the engine.
- * 
- * @author ynsbalci
- *
- */
+import shaders.StaticShader;
+ 
 public class MainGameLoop {
-
-
-	/**
-	 * Loads up the position data for two triangles (which together make a quad)
-	 * into a VAO. This VAO is then rendered to the screen every frame.
-	 * 
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		
-		DisplayManager.createDisplay();
-		Loader loader = new Loader();
-		Renderer renderer = new Renderer();
-		
-		float[] vertices = {
-				-0.5f, 0.5f, 0f,//v0
-				-0.5f, -0.5f, 0f,//v1
-				0.5f, -0.5f, 0f,//v2
-				0.5f, 0.5f, 0f,//v3
-		};
-		
-		int[] indices = {
-				0,1,3,//top left triangle (v0, v1, v3)
-				3,1,2//bottom right triangle (v3, v1, v2)
-		};
-		
-		RawModel model = loader.loadToVAO(vertices, indices);
-
-		while (!Display.isCloseRequested()) {
-			// game logic
-			renderer.prepare();
-			renderer.render(model);
-			DisplayManager.updateDisplay();
-		}
-
-		loader.cleanUp();
-		DisplayManager.closeDisplay();
-	}
-
+ 
+    public static void main(String[] args) {
+ 
+        DisplayManager.createDisplay();
+        Loader loader = new Loader();
+        Renderer renderer = new Renderer();
+        StaticShader shader = new StaticShader();
+         
+        float[] vertices = {            
+                -0.5f,0.5f,0,   //V0
+                -0.5f,-0.5f,0,  //V1
+                0.5f,-0.5f,0,   //V2
+                0.5f,0.5f,0     //V3
+        };
+         
+        int[] indices = {
+                0,1,3,  //Top left triangle (V0,V1,V3)
+                3,1,2   //Bottom right triangle (V3,V1,V2)
+        };
+         
+        RawModel model = loader.loadToVAO(vertices,indices);
+         
+        while(!Display.isCloseRequested()){
+            //game logic
+            renderer.prepare();
+            shader.start();
+            renderer.render(model);
+            shader.stop();
+            DisplayManager.updateDisplay();         
+        }
+ 
+        shader.cleanUp();
+        loader.cleanUp();
+        DisplayManager.closeDisplay();
+ 
+    }
+ 
 }
